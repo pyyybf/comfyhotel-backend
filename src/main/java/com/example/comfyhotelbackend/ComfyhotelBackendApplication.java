5397e5998ec154fe5609b4e67bc3842d68222873
panyue <pyyybf@gmail.com>
2022-09-20 15:43:07 -0700

    first commit: create springboot app

---
+++ src/main/java/com/example/comfyhotelbackend/ComfyhotelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.comfyhotelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComfyhotelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComfyhotelBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/comfyhotelbackend/ComfyhotelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comfyhotelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComfyhotelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
