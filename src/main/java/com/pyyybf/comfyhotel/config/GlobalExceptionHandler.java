4e415a2b8fe6cb663a9c26af47f7165ca628a223
panyue <pyyybf@gmail.com>
2022-09-22 01:28:04 -0700

    add exception handler

---
+++ src/main/java/com/pyyybf/comfyhotel/config/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.pyyybf.comfyhotel.config;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @program: comfy_hotel
+ * @description: handle the exception in controller
+ * @author: panyue
+ * @date: 2022/9/21
+ **/
+@ResponseBody
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
